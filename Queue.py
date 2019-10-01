fc2b536b72b7b0f5c6a08cd6e5b47f44070ebbb2
Dracarys <56050820+xdracaryS@users.noreply.github.com>
2019-10-02 01:49:06 +0300

    Update Queue.py

--- Queue.py
+++ Queue.py
@@ -9,12 +9,13 @@ class QueuePython(object):
 		if len(self.eventList) > 0:
 			self.eventList.clear()
 	def Process(self):
-		for j in xrange(len(self.eventList)):
-			if app.GetTime() > self.eventList[j]["time"]:
-				i = self.eventList[j]["func"]()
-				if i == 0:
-					self.eventList[j].clear()
-				else:
-					self.eventList[j]["time"] = app.GetTime()+i
+		if len(self.eventList) > 0:
+			for j in xrange(len(self.eventList)):
+				if app.GetTime() > self.eventList[j]["time"]:
+					i = self.eventList[j]["func"]()
+					if i == 0:
+						self.eventList[j].clear()
+					else:
+						self.eventList[j]["time"] = app.GetTime()+i
 	def AppendEvent(self,name,func,time):
 		self.eventList.append({"name":str(name),"func":__mem_func__(func),"time":int(app.GetTime()+time)})
