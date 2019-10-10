6ed20f65759a22191a23b50375b6edaeb23aad7a
Dracarys <56050820+xdracaryS@users.noreply.github.com>
2019-10-10 13:54:12 +0300

    Update Queue.py

--- Queue.py
+++ Queue.py
@@ -5,64 +5,76 @@
 #update
 # 2.10.2019 12:48
 
+# update 10.10.2019
 # Special thanks for vegaS
 
-#import types﻿
 class Queue(object):
-	EXIT = 0
-	def __init__(self):
-		self.eventList = []
-		self.eventLockedList = []
-	def __del__(self):
-		del self.eventList
-		del self.eventLockedList
-	def GetEvent(self, eventName):
-		""" Get the event dictionary by specific name. """
-		for event in self.eventList:
-			if event['name'] == eventName:
-				return event
-		return None
-	def GetIsLockedEvent(self, eventName):
-		""" Get a boolean if a specific event is locked. """
-		for event in self.eventLockedList:
-			if event['name'] == eventName:
-				return True
-		return False
-	def LockEvent(self, eventName):
-		""" Lock a specific event by name for being processed. """
-		event = self.GetEvent(eventName)
-		if event and not event in self.eventLockedList:
-			self.eventLockedList.append(event)
-	def UnlockEvent(self, eventName):
-		""" Unlock a specific event by name for being processed. """
-		event = self.GetEvent(eventName)
-		if event in self.eventLockedList:
-			self.eventLockedList.remove(event)
-	def DeleteEvent(self, eventName):
-		""" Delete a specific event by name instantly. """
-		event = self.GetEvent(eventName)
-		if event:
-			self.eventList.remove(event)
-	def AppendEvent(self, eventName, eventStartTime, eventFunc, eventFuncArgs = ()):
-		""" Append a new event by specific arguments. """
-		#if not eventName or not type(eventStartTime) == int or not isinstance(eventFunc, types.MethodType):
-		#	return﻿﻿
-		if self.GetEvent(eventName):
-			return
-		if not hasattr(type(eventFuncArgs), '__iter__'):
-			eventFuncArgs = tuple([eventFuncArgs])
-		self.eventList.append({'name' :str(eventName), 'function' : __mem_func__(eventFunc), 'arguments' : eventFuncArgs, 'run_next_time' : int(app.GetGlobalTimeStamp() + eventStartTime)})
-	def Process(self):
-		""" Processing the events. """
-		for index, event in enumerate(self.eventList):
-			if event in self.eventLockedList:
-				continue
-			if app.GetGlobalTimeStamp() > event['run_next_time']:
-				result = result = apply(event['function'], event['arguments'])
-				if result == self.EXIT:
-					del self.eventList[index]
-					continue
-				event['run_next_time'] = app.GetGlobalTimeStamp() + result
+    EXIT = 0
+    def __init__(self):
+        self.eventList = []
+        self.eventLockedList = []
+    def __del__(self):
+        del self.eventList
+        del self.eventLockedList
+    def GetEvent(self, eventName):
+        """ Get the event dictionary by specific name. """
+        for event in self.eventList:
+            if event['name'] == eventName:
+                return event
+        return None
+        
+    def GetIsLockedEvent(self, eventName):
+        """ Get a boolean if a specific event is locked. """
+        for event in self.eventLockedList:
+            if event['name'] == eventName:
+                return True
+        return False
+        
+    def LockEvent(self, eventName):
+        """ Lock a specific event by name for being processed. """
+        event = self.GetEvent(eventName)
+        if event and not event in self.eventLockedList:
+            self.eventLockedList.append(event)
+            
+    def UnlockEvent(self, eventName):
+        """ Unlock a specific event by name for being processed. """
+        event = self.GetEvent(eventName)
+        if event in self.eventLockedList:
+            self.eventLockedList.remove(event)
+            
+    def DeleteEvent(self, eventName):
+        """ Delete a specific event by name instantly. """
+        event = self.GetEvent(eventName)
+        if event:
+            self.eventList.remove(event)
+            
+    def ResetTimeEvent(self, eventName):
+        """ Reset time of a specific event by name. """
+        event = self.GetEvent(eventName)
+        if event:
+            index = self.eventList.index(event)
+            self.eventList[index] = app.GetGlobalTimeStamp()
+        
+    def AppendEvent(self, eventName, eventStartTime, eventFunc, eventFuncArgs = ()):
+        """ Append a new event by specific arguments. """
+        if not eventName or not isinstance(eventStartTime, int) or not callable(eventFunc):
+            return
+        if not hasattr(type(eventFuncArgs), '__iter__'):
+            eventFuncArgs = tuple([eventFuncArgs])
+        if self.GetEvent(eventName):
+            self.DeleteEvent(eventName)
+        self.eventList.append({'name' : eventName, 'function' : __mem_func__(eventFunc), 'arguments' : eventFuncArgs, 'run_next_time' : app.GetGlobalTimeStamp() + eventStartTime})
+    def Process(self):
+        """ Processing the events. """
+        for index, event in enumerate(self.eventList):
+            if event in self.eventLockedList:
+                continue
+            if app.GetGlobalTimeStamp() > event['run_next_time']:
+                result = apply(event['function'], event['arguments'])
+                if result in (None, self.EXIT):
+                    del self.eventList[index]
+                    continue
+                event['run_next_time'] = app.GetGlobalTimeStamp() + result
 
 event_create = Queue()
 import __builtin__
